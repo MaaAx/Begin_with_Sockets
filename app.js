015b4a32c010e5966c04e5fb475a16e4507eb13a
Maxime <maximelamaison@me.com>
2013-11-21 18:31:31 +0100

    heroku

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
-app.use(express.favicon(__dirname + '/public/favicon.png'));
+app.use(express.favicon(__dirname + '/public/favicon.ico'));
 app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req, res){
